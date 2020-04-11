914cf354d7c148145d79231ac945094df5483c07
AravindaAN <8654975+AravindaAN@users.noreply.github.com>
2020-04-11 12:47:33 -0400

    added MVN settings to PATH
    added MVN settings to PATH

--- Hello.bat
+++ Hello.bat
@@ -1,3 +1,4 @@
 echo "Hello world from batch file"
 echo %PATH%
+set PATH =%PATH%;C:\Program Files\Java\jdk-14\bin;C:\Users\Aravinda\Downloads\software\apache-maven-3.6.3-bin\apache-maven-3.6.3\bin;
 mvn clean
