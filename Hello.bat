95961bc226fa10c870d01ff571c40b0adc697311
AravindaAN <8654975+AravindaAN@users.noreply.github.com>
2020-04-11 13:09:46 -0400

    aaas

--- Hello.bat
+++ Hello.bat
@@ -1,5 +1,11 @@
 echo "Hello world from batch file"
 
 set PATH =%PATH%;C:\Program Files\Java\jdk-14\bin;C:\Users\Aravinda\Downloads\software\apache-maven-3.6.3-bin\apache-maven-3.6.3\bin;
+environment {
+        MAVEN = 'C:\Users\Aravinda\Downloads\software\apache-maven-3.6.3-bin\apache-maven-3.6.3\bin'
+        JDK_HOME    = 'C:\Program Files\Java\jdk-14'
+    }
 echo %PATH%
-mvn clean
+${MAVEN}\mvn
+
+
